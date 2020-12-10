8986a26de7d95e273fa77769abe613fe5d702e9c
Andreas Svedstedt <andreas@poema.se>
2020-12-10 17:32:16 +0100

    måtten fixade också

--- app/src/main/java/com/poema/andreasmvvm/activities/ChosenDrinkActivity.kt
+++ app/src/main/java/com/poema/andreasmvvm/activities/ChosenDrinkActivity.kt
@@ -18,11 +18,12 @@ class ChosenDrinkActivity : AppCompatActivity() {
         val drinkName = Datamanager.drinks[pos].strDrink
         tv_name.text = drinkName
         showImage(pos)
-        showIngredients(pos)
+        getNotNullIngredients(pos)
         showInstructions(pos)
+        getNotNullMeasurements(pos)
     }
 
-    private fun showIngredients(pos:Int) {
+    private fun getNotNullIngredients(pos:Int) {
         var ingredStr = ""
         if (Datamanager.drinks[pos].strIngredient1 != null){ ingredStr += "${Datamanager.drinks[pos].strIngredient1}\n"}
         if (Datamanager.drinks[pos].strIngredient2 != null){ ingredStr += "${Datamanager.drinks[pos].strIngredient2}\n"}
@@ -37,14 +38,33 @@ class ChosenDrinkActivity : AppCompatActivity() {
         tv_ingred.text = ingredStr
     }
 
+    private fun getNotNullMeasurements(pos:Int) {
+        var measurementStr = ""
+        if (Datamanager.drinks[pos].strMeasure1 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure1}\n"}
+        if (Datamanager.drinks[pos].strMeasure2 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure2}\n"}
+        if (Datamanager.drinks[pos].strMeasure3 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure3}\n"}
+        if (Datamanager.drinks[pos].strMeasure4 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure4}\n"}
+        if (Datamanager.drinks[pos].strMeasure5 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure5}\n"}
+        if (Datamanager.drinks[pos].strMeasure6 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure6}\n"}
+        if (Datamanager.drinks[pos].strMeasure7 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure7}\n"}
+        if (Datamanager.drinks[pos].strMeasure8 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure8}\n"}
+        if (Datamanager.drinks[pos].strMeasure9 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure9}\n"}
+        if (Datamanager.drinks[pos].strMeasure10 != null) {measurementStr += "${Datamanager.drinks[pos].strMeasure10}\n"}
+
+        tv_measure.text = measurementStr
+    }
+
 
     private fun showImage(pos: Int) {
         Glide.with(this)
             .load(Datamanager.drinks[pos].strDrinkThumb).apply(RequestOptions.circleCropTransform())
             .into(iv_picture)
     }
+
+
     private fun showInstructions(pos: Int){
         tv_instructions.text = Datamanager.drinks[pos].strInstructions
     }
+
 
 }
